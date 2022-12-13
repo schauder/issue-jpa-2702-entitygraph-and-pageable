4d2bf9c4772e26abadd8d468b70864bfd5a53c5a
Jens Schauder <jens@schauderhaft.de>
2022-12-13 14:32:50 +0100

    boot 3.0.0 fails.

--- src/main/java/com/example/demo/SomeThing.java
+++ src/main/java/com/example/demo/SomeThing.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 
